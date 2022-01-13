f5b8abad8851bc40f5ee6c403bfe6e82a49d87cb
pgates-test <Janick5234xz>
2022-01-13 18:09:27 +0100

    zmiana na mnożenie

---
+++ cmake-build-debug/CMakeFiles/git_lab.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/git_lab.dir/main.cpp.obj"
+  "git_lab.exe"
+  "git_lab.exe.manifest"
+  "git_lab.pdb"
+  "libgit_lab.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/git_lab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
